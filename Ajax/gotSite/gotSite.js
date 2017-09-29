3da6fb1a21055d41a8227e748f2af9b0aa6770b8
KennyTheWizard <kenngineering@gmail.com>
2017-09-29 11:20:30 -0500

    added weather and game of thrones assignment

---
+++ Ajax/gotSite/gotSite.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    for (var index = 0; index < 400; index++) {
+        $.get('https://anapioficeandfire.com/api/houses/' + index,function(){
+            
+        })
+    }
+})
